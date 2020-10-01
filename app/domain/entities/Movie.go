e238213334b3d7653e43236cd1d03d180d16532d
Marcio Robson da Silva da Penha <robsonmrsp@gmail.com>
2020-09-30 22:30:32 -0300

    Define PagerMovie.

--- app/domain/entities/Movie.go
+++ app/domain/entities/Movie.go
@@ -4,12 +4,19 @@ import "time"
 
 // PagerMovie ...
 type PagerMovie struct {
+	Items        *[]Movie `json:"items"`
+	ActualPage   int      `json:"actualPage"`
+	TotalRecords int32    `json:"totalRecords"`
+
+	PageSize int    `json:"pageSize"`
+	OrderBy  string `json:"orderBy"`
+	Order    string `json:"order"`
 }
 
 // Movie ...
 type Movie struct {
-	ID          int       `db:"id" `
-	Title       string    `db:"name"`
-	ReleaseDate time.Time `db:"release_date"`
-	Genre       *Genre
+	ID          int       `db:"id" json:"id"`
+	Title       string    `db:"name" json:"title"`
+	ReleaseDate time.Time `db:"release_date" json:"releaseDate"`
+	Genre       *Genre    `db:"genre_id" json:"genre"`
 }
