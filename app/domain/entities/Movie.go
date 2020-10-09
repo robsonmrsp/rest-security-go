0cce76c177b04d6735ef42e0c2e6fd93d7d8910c
Marcio Robson da Silva da Penha <robsonmrsp@gmail.com>
2020-10-08 22:20:17 -0300

    create a CustomDate and a CustomTime to best serialize the dates

--- app/domain/entities/Movie.go
+++ app/domain/entities/Movie.go
@@ -1,6 +1,49 @@
 package entities
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type CustomTime time.Time
+type CustomDate time.Time
+
+const customTimeLayout = "2006-01-02 15:04:05"
+const customDateLayout = "2006-01-02"
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), `"`)
+	nt, err := time.Parse(customTimeLayout, s)
+	*ct = CustomTime(nt)
+	return
+}
+
+func (ct CustomTime) MarshalJSON() ([]byte, error) {
+	return []byte(ct.String()), nil
+}
+
+func (ct *CustomTime) String() string {
+	t := time.Time(*ct)
+	return fmt.Sprintf("%q", t.Format(customTimeLayout))
+}
+
+func (ct *CustomDate) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), `"`)
+	nt, err := time.Parse(customDateLayout, s)
+	*ct = CustomDate(nt)
+	return
+}
+
+func (ct CustomDate) MarshalJSON() ([]byte, error) {
+	return []byte(ct.String()), nil
+}
+
+// String returns the time in the custom format
+func (ct *CustomDate) String() string {
+	t := time.Time(*ct)
+	return fmt.Sprintf("%q", t.Format(customDateLayout))
+}
 
 // PagerMovie ...
 type PagerMovie struct {
@@ -15,8 +58,8 @@ type PagerMovie struct {
 
 // Movie ...
 type Movie struct {
-	ID          int       `db:"id" json:"id"`
-	Title       string    `db:"name" json:"title"`
-	ReleaseDate time.Time `db:"release_date" json:"releaseDate"`
-	Genre       *Genre    `db:"genre_id" json:"genre"`
+	ID          int        `db:"id" json:"id"`
+	Title       string     `db:"name" json:"title"`
+	ReleaseDate CustomDate `db:"release_date" json:"releaseDate"`
+	Genre       *Genre     `db:"genre_id" json:"genre"`
 }
